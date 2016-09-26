653df6a752e950f038da557601af71574b10b8f2
shouzo <danny200026@yahoo.com.tw>
2016-09-26 21:38:56 +0800

    20160926 Update

---
+++ android-class/20160926/HelloWorld2/app/src/main/java/com/omomys/helloworld/MainActivity.java
@@ -0,0 +1,83 @@
+package com.omomys.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnCreate() is called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnStart() is called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnRestart() is called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnResume() is called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnPause() is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onPause();
+        Toast.makeText(this, "onStop() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnStop() is called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy() is called", Toast.LENGTH_SHORT).show();
+        Log.i("HelloWorld", "OnDestroy() is called");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
